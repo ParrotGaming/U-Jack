38761ed85eeb770db696cd3bbe29553219b4cf11
Ravneet Bhatia <rsbhatia2@usfca.edu>
2024-04-06 17:35:58 -0700

    adding image logic

--- app/src/main/java/com/wompwompstudios/u_jack/ReportActivity.kt
+++ app/src/main/java/com/wompwompstudios/u_jack/ReportActivity.kt
@@ -2,8 +2,11 @@ package com.wompwompstudios.u_jack
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
@@ -16,30 +19,47 @@ class ReportActivity : AppCompatActivity(){
         // Initialize Firebase Auth
         auth = FirebaseAuth.getInstance()
         val database = Firebase.firestore
-        //Verify User is logged in
+
         if(auth.currentUser == null) {
             startActivity(Intent(this, LogInActivity::class.java))
         }
-        //Content view
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report)
-        //View references
+
         val btnSubmitReport = findViewById<Button>(R.id.btnSubmitReport)
         val etUserDescription = findViewById<EditText>(R.id.UserCarDescriptionInput)
         val etEstimatedValue = findViewById<EditText>(R.id.EstimatedCarValueInput)
         val etDifficultyRating = findViewById<EditText>(R.id.DifficultyRatingStatementInput)
-        //Btn submit logic
+        val carImageView = findViewById<ImageView>(R.id.CarImage)
+        val carImagebtn = findViewById<Button>(R.id.btnAddImage)
+
+        val changeImage =
+            registerForActivityResult(
+                ActivityResultContracts.StartActivityForResult()
+            ) {
+                if (it.resultCode == Activity.RESULT_OK) {
+                    val data = it.data
+                    val imgUri = data?.data
+                    carImageView.setImageURI(imgUri)
+                }
+            }
+
+        carImagebtn.setOnClickListener {
+            val picking = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            changeImage.launch(picking)
+        }
+
+
         btnSubmitReport.setOnClickListener() {
-            //validate input
             if(Integer.parseInt(etDifficultyRating.text.toString()) in 1..10) {
-                //create car object
                 val car = hashMapOf(
                     "User" to auth.currentUser!!.uid,
                     "Description" to etUserDescription.text.toString(),
                     "Value" to etEstimatedValue.text.toString(),
                     "Difficulty" to etDifficultyRating.text.toString(),
                 )
-                //push to database
+
                 database.collection("cars")
                     .add(car)
                     .addOnSuccessListener { documentReference ->
