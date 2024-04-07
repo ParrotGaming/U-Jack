6414f5c5a0c1dc97caa8ebbff0abc5fcec281f3c
Alexis Murray <alexjacobmurray@gmail.com>
2024-04-06 19:07:01 -0700

    deleted old activity files that have been converted to fragments and updated manifest

--- app/src/main/java/com/wompwompstudios/u_jack/LogbookFragment.kt
+++ app/src/main/java/com/wompwompstudios/u_jack/LogbookFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+private const val TESTVAR = "test"
 
 /**
  * A simple [Fragment] subclass.
